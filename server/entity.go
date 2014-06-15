e790e34f1f54ea2459130592a3d178653cb96e14
Matt Basta <me@mattbasta.com>
2014-06-14 18:13:13 -0700

    Reformatting

--- server/entity.go
+++ server/entity.go
@@ -1,56 +1,56 @@
 package server
 
 import (
-	"strconv"
+    "strconv"
 
-	"legend-of-adventure/server/terrain"
+    "legend-of-adventure/server/terrain"
 )
 
 var entityNameCounter = 0
 
 func NextEntityID() string {
-	name := "e" + strconv.Itoa(entityNameCounter)
-	entityNameCounter++
-	return name
+    name := "e" + strconv.Itoa(entityNameCounter)
+    entityNameCounter++
+    return name
 }
 
 type Entity interface {
-	Receive() chan<- *Event
-	Setup()
-	Killer(chan<- bool) // Used to notify entity it is being destroyed
+    Receive() chan<- *Event
+    Setup()
+    Killer(chan<- bool) // Used to notify entity it is being destroyed
 
-	ID() string
-	Position() (float64, float64)
-	Size() (uint, uint)
+    ID() string
+    Position() (float64, float64)
+    Size() (uint, uint)
 
-	Dead() bool
+    Dead() bool
 
-	Location() *Region
-	Inventory() *Inventory // May return nil
+    Location() *Region
+    Inventory() *Inventory // May return nil
 
-	GetIntroduction() string // Entity add command's body
+    GetIntroduction() string // Entity add command's body
 
-	String() string
+    String() string
 }
 
 type Animat interface {
-	Entity
+    Entity
 
-	// Properties() (int, string, string) // Layer, image, view
+    // Properties() (int, string, string) // Layer, image, view
 
-	Direction() (int, int)
-	Velocity() (int, int)
+    Direction() (int, int)
+    Velocity() (int, int)
 
-	// ShouldWeightDirections() bool
-	MovementEffect() string
+    // ShouldWeightDirections() bool
+    MovementEffect() string
 
-	GetHealth() uint
-	IsAtMaxHealth() bool
-	IncrementHealth(amount uint)
+    GetHealth() uint
+    IsAtMaxHealth() bool
+    IncrementHealth(amount uint)
 }
 
 type SentientAnimat interface {
-	Animat
+    Animat
 }
 
 func IsEntityCollidingWithPortal(portal terrain.Portal, entity Entity) bool {
@@ -58,7 +58,7 @@ func IsEntityCollidingWithPortal(portal terrain.Portal, entity Entity) bool {
     ew, eh := entity.Size()
     ewT, ehT := float64(ew / TILE_SIZE), float64(eh / TILE_SIZE)
     return (ex + ewT >= float64(portal.X) &&
-    		float64(portal.X + portal.W) >= ex &&
-    		ey + ehT >= float64(portal.Y) &&
-    		float64(portal.Y + portal.H) >= ey)
+            float64(portal.X + portal.W) >= ex &&
+            ey >= float64(portal.Y) &&
+            float64(portal.Y + portal.H) >= ey - ehT)
 }
