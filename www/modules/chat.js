e4c4d0667c7fee3f9a3c605f2b3b680862b35a5b
Matt Basta <me@mattbasta.com>
2014-08-09 17:04:24 -0700

    Regionalized chat

--- www/modules/chat.js
+++ www/modules/chat.js
@@ -1,39 +1,51 @@
-define('chat', ['comm', 'keys', 'level'], function(comm, keys, level) {
+define('chat', ['comm', 'entities', 'keys', 'level'], function(comm, entities, keys, level) {
     'use strict';
+
+    var CHAT_DISTANCE = 10;
 
     var started = false;
 
-    var chatbox = document.getElementById("chatbox");
+    var chatbox = document.getElementById('chatbox');
     var textbox = document.getElementById('talkbar');
 
     function handleMessage(message) {
         if(chatbox.childNodes.length > 10) {
             chatbox.removeChild(chatbox.childNodes[0]);
         }
-        var p = document.createElement("p");
-        if(message[0] == "/")
-            p.style.color = "#5d6";
+        var p = document.createElement('p');
+        if(message[0] == '/')
+            p.style.color = '#5d6';
         p.innerHTML = message;
         chatbox.appendChild(p);
     }
 
     comm.messages.on('cha', function(body) {
-        // TODO: Make this handle the X and Y coords.
-        handleMessage(body.substr(body.indexOf('\n') + 1))
+        var breakIdx = body.indexOf('\n');
+
+        // Ignore chat messages that come from too far away.
+        var coords = body.substr(0, breakIdx).split(' ');
+        var local = entities.getLocal();
+        var dist = Math.sqrt(
+            Math.pow(local.x - coords[0], 2),
+            Math.pow(local.y - coords[1], 2)
+        );
+        if (dist > CHAT_DISTANCE) return;
+
+        handleMessage(body.substr(breakIdx + 1))
     });
 
     function stopChat() {
         started = false;
-        textbox.value = "";
-        textbox.style.display = "none";
-        chatbox.style.bottom = "100px";
+        textbox.value = '';
+        textbox.style.display = 'none';
+        chatbox.style.bottom = '100px';
     }
 
     level.on('pause', stopChat);
 
     var ret = {
         startChat : function() {
-            textbox.style.display = "block";
+            textbox.style.display = 'block';
             setTimeout(function() {textbox.focus();}, 0);
             textbox.onkeydown = function(e) {
                 e.stopPropagation();
@@ -41,7 +53,7 @@ define('chat', ['comm', 'keys', 'level'], function(comm, keys, level) {
                     case 13:
                         var m = textbox.value;
                         if(m) {
-                            comm.send("cha", m);
+                            comm.send('cha', m);
                             handleMessage(m);
                         }
                     case 27:
@@ -53,7 +65,7 @@ define('chat', ['comm', 'keys', 'level'], function(comm, keys, level) {
                 e.stopPropagation();
             };
             started = true;
-            chatbox.style.bottom = "130px";
+            chatbox.style.bottom = '130px';
             return false;
         },
         stopChat: stopChat
